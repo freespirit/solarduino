d886eb6c3a1819ded199a53ecb876b10f1d686df
Stanislav Trifonov <stan.trifonov@gmail.com>
2015-01-26 23:56:15 +0200

    - Use radians for the trigonometric functions arguments - Put RA in 0-360 degrees range

--- TestCoordinates/src/st/coordinates/Coordinates.java
+++ TestCoordinates/src/st/coordinates/Coordinates.java
@@ -23,31 +23,43 @@ public class Coordinates {
 		L = L - times360*360.0;
 		times360 = (int) (g / 360.0);
 		g = g - times360*360.0;
+		times360 = (int) ((2*g) / 360.0);
+		double g2 = 2*g - times360;
 		
-		double lambda = L + 1.915*Math.sin(g) + 0.020*Math.sin(2*g); // ecliptic longitude of the Sun
+		// ecliptic longitude of the Sun
+		double lambda = L +
+				1.915*Math.sin( Math.toRadians(g) ) +
+				0.020*Math.sin( Math.toRadians(g2) );
 
 
 		//  2. convert to the equatorial coordinate system, and
 		double epsilon = 23.439 - 0.0000004*n;//approximation of obliquity of the ecliptic
-		double RA = Math.atan( Math.cos(epsilon) * Math.tan(lambda) );
-		double Dec = Math.asin( Math.sin(epsilon) * Math.sin(lambda) );
+		
+		double lambdaRads = Math.toRadians(lambda);
+		double epsilonRads = Math.toRadians(epsilon);
+
+		double RA = Math.atan(
+				Math.cos( epsilonRads ) *
+				Math.tan( lambdaRads )
+				);
+		double Dec = Math.asin(
+				Math.sin( epsilonRads ) *
+				Math.sin( lambdaRads )
+				);
+		
+		RA = Math.toDegrees(RA);
+		Dec = Math.toDegrees(Dec);
+		
+		while(RA < 0)
+			RA += 360.0;
+		
+		times360 = (int) (RA / 360.0);
+		RA -= times360 * 360.0;
 		System.out.println("RA: " + RA + " DEC: " + Dec);
 
 
 		//  3. convert to the horizontal coordinate system, for the observer's local time and position.
 
-	  
-	  
-//	  Serial.print("JD: ");
-//	  Serial.print(JD);
-//	  Serial.print(" RA: ");
-//	  Serial.print(RA);
-//	  Serial.print(" DEC: ");
-//	  Serial.print(DEC);
-//	  Serial.print(" ALT: ");
-//	//  Serial.print(ALT);
-//	  Serial.print(" AZ: ");
-//	//  Serial.println(AZ);
 	}
 
 
